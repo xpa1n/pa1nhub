5bcc957bde77cc8988f2e2d8364f6d2e3c9b121c
xpa1n <97103648+xpa1n@users.noreply.github.com>
2022-01-19 09:47:07 +0600

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,41 @@
+--- DEMONFALL
+if game.PlaceId == 5094651510 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xpa1n/pa1nhub/main/demonfall.lua"))()
+else
+    print("x")
+end
+
+--- PET SIMULATOR X
+if game.PlaceId == 6284583030 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xpa1n/pa1nhub/main/PSX.lua"))()
+else
+    print("x")
+end
+
+--- BLOX FRUITS
+if game.PlaceId == 2753915549 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xpa1n/pa1nhub/main/BloxFruits.lua"))()
+else
+    print("x")
+end
+
+--- YOUR BIZARRE ADVENTURE
+if game.PlaceId == 2809202155 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xpa1n/pa1nhub/main/YBA.lua"))()
+else
+    print("x")
+end
+
+--- SHINDO LIFE
+if game.PlaceId == 6986372023 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xpa1n/pa1nhub/main/ShindoLife.lua"))()
+else
+    print("x")
+end
+
+--- ANIME FIGHTERS SIMULATOR
+if game.PlaceId == 6299805723 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xpa1n/pa1nhub/main/AFS.lua"))()
+else
+    print("x")
+end
